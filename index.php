93c8d3950416509de78110555b6c819dab3f5d8b
Nzingani <isabelnzingani@outlook.com>
2018-07-25 21:32:24 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>exo2</title>
+  </head>
+  <body>
+  <p>
+    <?php
+    $lastname = 'nzingani';
+    $firstname = 'isabel';
+    $age = 35;
+    echo $lastname . ' ' . $firstname . ' ' . $age;;
+    ?>
+  </p>
+  </body>
+</html>
